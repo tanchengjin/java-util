7e81ff47be1da988d91295f74647fd0d3804cbc9
tanchengjin <18865477815@163.com>
2022-01-14 19:55:40 +0800

    StringUtil

--- src/main/java/com/tanchengjin/utils/StringUtil.java
+++ src/main/java/com/tanchengjin/utils/StringUtil.java
@@ -4,6 +4,7 @@ package com.tanchengjin.utils;
  * @author tanchengjin
  * @since v1.0.0
  */
+
 public class StringUtil {
     public static boolean isEmpty(CharSequence var) {
         return var == null || var.length() == 0;
